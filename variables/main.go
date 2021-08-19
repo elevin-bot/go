93e8f0ea3a507da94bc40026404548dd817c24c8
Eugene Levin <jenya.levin@gmail.com>
2021-08-19 23:04:39 +1000

    Test

--- variables/main.go
+++ variables/main.go
@@ -8,5 +8,12 @@ import (
 func main() {
 	var i int = 10
 	j := strconv.Itoa(i)
-	fmt.Printf("%v, %T", j, j)
+	var r [2]int
+	r[0] = 5
+	r[1] = 6
+
+	const a = 45
+	fmt.Printf("%v, %T\n", j, j)
+	fmt.Printf("%v, %T\n", r[1], r)
+	fmt.Printf("%v, %T", a, a)
 }
